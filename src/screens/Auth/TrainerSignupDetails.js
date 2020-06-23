9fd2fb2942f8496f6bdb667af2ce404af97547d2
yogeshpeculiar <43494870+yogeshpeculiar@users.noreply.github.com>
2020-06-23 14:45:22 +0530

    trainerDetailsScreen Update

--- src/screens/Auth/TrainerSignupDetails.js
+++ src/screens/Auth/TrainerSignupDetails.js
@@ -1,12 +1,14 @@
-import React, {useState, Component} from 'react';
-import {Text, TouchableOpacity, StyleSheet, TextInput, View, Image} from 'react-native';
-import {addTrainerDetails} from '../../API';
-// import * as ImagePicker from 'expo-image-picker';
+import React, { useState, Component } from 'react';
+import { Text, TouchableOpacity, StyleSheet, TextInput, View, Image, StatusBar } from 'react-native';
+import { addTrainerDetails } from '../../API';
 import ImagePicker from 'react-native-image-picker';
 import defaultPic from '../../../assets/male_pic_default.jpg';
-import {uploadImage} from '../../API';
-import {connect} from "react-redux";
+import { uploadImage } from '../../API';
+import { connect } from "react-redux";
 import * as actionCreators from "../../store/actions";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import FormElementTwo from '../../components/Signup/FormElement';
+import ActionButtonTwo from '../../components/Login/ActionButtonTwo';
 
 class TrainerSignupDetails extends Component {
 
@@ -21,12 +23,12 @@ class TrainerSignupDetails extends Component {
     }
   }
 
-   pickImage() {
+  pickImage() {
     const options = {
     };
-    ImagePicker.showImagePicker( options , (response) => {
+    ImagePicker.showImagePicker(options, (response) => {
       console.log('Response = ', response);
-     
+
       if (response.didCancel) {
         console.log('User cancelled image picker');
       } else if (response.error) {
@@ -34,15 +36,15 @@ class TrainerSignupDetails extends Component {
       } else if (response.customButton) {
         console.log('User tapped custom button: ', response.customButton);
       } else {
-        console.log('image url----------'+JSON.stringify(response.uri))
+        console.log('image url----------' + JSON.stringify(response.uri))
         this.setState({
           image: response.uri,
         });
-        console.log('image modified url----------'+JSON.stringify(this.state.image))
+        console.log('image modified url----------' + JSON.stringify(this.state.image))
       }
     });
   };
-  
+
 
 
   async submitPhoto(path, token) {
@@ -55,130 +57,54 @@ class TrainerSignupDetails extends Component {
 
   async submit() {
 
-    // if (this.state.height != 0) {
-    //   theight = JSON.stringify(this.state.height.text);
-    //   theight = theight.slice(1, -1);
-    // }
-    // if (this.state.weight != 0) {
-    //   tweight = JSON.stringify(this.state.weight.text);
-    //   tweight = tweight.slice(1, -1);
-    // }
-    // if (this.state.experience != 0) {
-    //   texp = JSON.stringify(this.state.experience.text);
-    //   texp = texp.slice(1, -1);
-    // }
-    if (this.state.name != '') {
-      tname = JSON.stringify(this.state.name.text);
-      tname = tname.slice(1, -1);
-    }
-    var result = await addTrainerDetails(tname);
+    var result = await addTrainerDetails(this.state.height.text, this.state.height.weight, this.state.height.experience, this.state.height.name);
     if (result) {
       console.log('addTrainerdetails----------' + result)
-      this.props.nav
-      this.props.setInitialLoginOff();
+      this.props.setAuthenticated(true);
     } else
       console.log('addTrainerdetails----------' + result)
 
-    // const state = store.getState();
-    // console.log('image modified url----------'+JSON.stringify(this.state.image))
-    // this.submitPhoto(this.state.image, state.jwt);
+    const state = store.getState();
+    console.log('image modified url----------' + JSON.stringify(this.state.image))
+    this.submitPhoto(this.state.image, state.jwt);
   }
 
 
   render() {
-   
+
 
     return (
+      <>
+        <StatusBar backgroundColor={'white'} />
+        <KeyboardAwareScrollView enableOnAndroid={true} contentContainerStyle={styles.contentContainer}>
+          <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
+            <TouchableOpacity onPress={() => {
+              this.pickImage(), console.log(this.state.image)
+            }} style={styles.addPhoto}>
+              {!this.state.image && <Image source={defaultPic} style={{ width: 200, height: 200, borderRadius: 100 }} />}
+              {this.state.image &&
+                <Image source={{ uri: this.state.image }} style={{ width: 200, height: 200, borderRadius: 100 }} />}
+            </TouchableOpacity>
+          </View>
+          <View style={{ flex: 3, width: "60%", marginLeft: 100 }}>
+            <FormElementTwo placeholder="Name" onChangeText={(text) => { this.setState({ name: { text } }) }} />
+            {/* <FormElementTwo placeholder="Height (in cms)" maxLength={4} keyboardType='numeric' onChangeText={(text) => { this.setState({ height: { text } }) }} />
+            <FormElementTwo placeholder="Weight (in Kgs)" maxLength={4} keyboardType='numeric' onChangeText={(text) => { this.setState({ weight: { text } }) }} />
+            <FormElementTwo placeholder="Experience" keyboardType='numeric' onChangeText={(text) => { this.setState({ experience: { text } }) }} /> */}
+          </View>
+          <View style={{ flex: 1, marginRight: 20, marginLeft: 20 }}>
+            <ActionButtonTwo label="submit"></ActionButtonTwo>
+          </View>
 
-      <View style={styles.container}>
-
-        <TouchableOpacity onPress={() => {
-          this.pickImage(), console.log(this.state.image)
-        }} style={styles.addPhoto}>
-          {!this.state.image && <Image source={defaultPic} style={{width: 200, height: 200, borderRadius: 100}}/>}
-          {this.state.image &&
-          <Image source={{uri: this.state.image}} style={{width: 200, height: 200, borderRadius: 100}}/> } 
-        </TouchableOpacity>
-
-
-        <View style={styles.detailElement}>
-          <Text style={styles.label}>Name </Text>
-          <TextInput style={styles.Input} onChangeText={(text) => {
-            this.setState({name: {text}})
-          }}></TextInput>
-        </View>
-
-        {/*<View style={styles.detailElement}>*/}
-        {/*  <Text style={styles.label}>Height </Text>*/}
-        {/*  <Text style={styles.note}>(in feet)</Text>*/}
-        {/*  <TextInput style={styles.Input} maxLength={4} keyboardType='numeric' onChangeText={(text) => {*/}
-        {/*    this.setState({height: {text}})*/}
-        {/*  }}> </TextInput>*/}
-        {/*</View>*/}
-        {/*<View style={styles.detailElement}>*/}
-        {/*  <Text style={styles.label}>weight </Text>*/}
-        {/*  <Text style={styles.note}>(in Kgs)</Text>*/}
-        {/*  <TextInput style={styles.Input} maxLength={4} keyboardType='numeric' onChangeText={(text) => {*/}
-        {/*    this.setState({weight: {text}})*/}
-        {/*  }}> </TextInput>*/}
-        {/*</View>*/}
-        {/*<View style={[styles.detailElement]}>*/}
-        {/*  <Text style={styles.label}>Experience</Text>*/}
-        {/*  <TextInput style={styles.Input} maxLength={4} keyboardType='numeric' onChangeText={(text) => {*/}
-        {/*    this.setState({experience: {text}})*/}
-        {/*  }}> </TextInput>*/}
-        {/*</View>*/}
-        <TouchableOpacity onPress={() => this.submit()} style={styles.submit}>
-          <Text style={styles.buttonLabel}>Submit</Text>
-        </TouchableOpacity>
-
-      </View>
-    );
+        </KeyboardAwareScrollView>
+      </>
+      );
   }
 }
 const styles = StyleSheet.create({
-  Input: {
-    width: 70,
-    borderBottomColor: 'grey',
-    borderBottomWidth: 1,
-    fontSize: 22,
-    textAlign: 'center',
-    marginLeft: 30,
-    marginTop: -10
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white'
-  },
-  detailElement: {
-    flexDirection: 'row',
-    marginTop: 20,
-    marginBottom: 20
-  },
-  label: {
-    fontSize: 24
-  },
-  note: {
-    fontSize: 18,
-    color: 'grey',
-    marginTop: 10,
-
-  },
-  submit: {
-    backgroundColor: 'blue',
-    borderRadius: 25,
-    width: '25%',
-    alignItems: 'center',
-    marginLeft: "35%",
-    padding: 5,
-    marginTop: 20,
-    marginRight: 150
-  },
-  buttonLabel: {
-    color: 'white',
-    fontSize: 22
+  contentContainer: {
+    flexGrow: 1,
+    backgroundColor: 'white',
 
   }
 
@@ -188,8 +114,7 @@ const styles = StyleSheet.create({
 const mapStateToProps = (state) => ({});
 
 const mapDispatchToProps = (dispatch) => ({
-  setAuthenticated: (value) => dispatch(actionCreators.setAuthenticated(value)),
-  setInitialLoginOff: ()=>dispatch(actionCreators.setInitialLoginOff())
+  setAuthenticated: (value) => dispatch(actionCreators.setAuthenticated(value))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TrainerSignupDetails);
